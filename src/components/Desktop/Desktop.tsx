77eeef1249242478173e884eecbcd450afcf1074
yurishe0 <yurishe00@gmail.com>
2025-02-06 13:57:12 +0100

    feat: utilize the context to display open applications

--- src/components/Desktop/Desktop.tsx
+++ src/components/Desktop/Desktop.tsx
@@ -1,7 +1,12 @@
 import styles from './desktop.module.scss'
 import DesktopIcon from '../DesktopIcon/DesktopIcon'
+import { useContext } from 'react';
+import OpenAppsContext from '../../contexts/OpenAppsContext';
+import Window from '../Window/Window';
 
 function Desktop() {
+    const openApps = useContext(OpenAppsContext);
+
     const handleDragOver = (e: React.DragEvent) => {
         e.preventDefault();
     }
@@ -11,6 +16,11 @@ function Desktop() {
             <DesktopIcon name="Test" />
             <DesktopIcon iconURL="https://cdn.iconscout.com/icon/free/png-256/free-email-icon-download-in-svg-png-gif-file-formats--envenlope-letter-mail-user-interface-pack-icons-83578.png" name="Very long iconname is it gdassssssssssssssssonna breddddddddddddddddddddddddddddddddddddak" />
             <DesktopIcon name="Very long iconname is it gonna break" />
+            {
+                openApps.map(app => {
+                    return <Window name={app.name} />
+                })
+            }
         </div>
     )
 }
