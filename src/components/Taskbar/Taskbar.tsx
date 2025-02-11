862d5dbb4b12bd491c4865d9e0b1d8b69fd17c3a
yurishe0 <yurishe00@gmail.com>
2025-02-11 12:10:01 +0100

    feat: show processes on taskbar

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -1,14 +1,36 @@
+import { useContext } from 'react';
+import AppContext from '../../contexts/AppContext';
 import styles from './taskbar.module.scss'
 import { TbPlant2 } from "react-icons/tb";
+import { Application } from '../../types/Application';
 
 function Taskbar() {
+    const { openApps } = useContext(AppContext);
+
     return (
         <div data-testid="taskbar" id="taskbar" className={styles.taskbar}>
             <StartMenu />
-            <div className="tabs" data-testid="tabs">
-
+            <div className={styles.tabs} data-testid="tabs">
+                { openApps.map(app => {
+                    return <Tab app={app} />
+                })}
             </div>
         </div>
+    )
+}
+
+function Tab({ app }: { app: Application }) {
+    return (
+        <button key={app.id} className={styles.tab}>
+            <figure>
+                <picture>
+                    <img src={app.iconURL} />
+                </picture>
+                <figcaption>
+                    {app.name}
+                </figcaption>
+            </figure>
+        </button>
     )
 }
 
