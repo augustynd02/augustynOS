e95527f2621f0d0c87352bf4028eaca2c7cb96fd
yurishe0 <yurishe00@gmail.com>
2025-02-14 12:30:57 +0100

    test: add taskbar test

--- src/components/Taskbar/Taskbar.test.tsx
+++ src/components/Taskbar/Taskbar.test.tsx
@@ -1,16 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Taskbar from './Taskbar';
+import Desktop from "../Desktop/Desktop";
+import DesktopIcon from "../DesktopIcon/DesktopIcon";
+import AppProvider from "../../contexts/AppProvider";
 
-describe('MyComponent', () => {
-    beforeEach(() => {
-        render(<Taskbar />);
-    })
+describe('Taskbar', () => {
+    it("renders tabs from open apps", () => {
+        render(
+            <AppProvider>
+                <Desktop>
+                    <DesktopIcon name="Test" type="test"/>
+                </Desktop>
+                <Taskbar />
+            </AppProvider>
+        );
 
-    it('renders the start menu', () => {
-        expect(screen.getByTestId('startmenu')).toBeInTheDocument();
-    });
+        const tabs = screen.getByTestId('tabs');
+        expect(tabs.children).toHaveLength(0);
 
-    it('renders the tabs', () => {
-        expect(screen.getByTestId('tabs')).toBeInTheDocument();
+        const icon = screen.getByTestId('desktopicon');
+        fireEvent.dblClick(icon);
+
+        expect(tabs.children).toHaveLength(1);
     })
 });
