fd5502d2e1a13b334c56dff6b0a3026381199a11
yurishe0 <yurishe00@gmail.com>
2025-02-15 13:51:25 +0100

    fix: use an unique identifier as Date has a chance to create identical IDs

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -2,7 +2,7 @@ import { Application } from "../types/Application";
 
 const createApp = (name: string, type: string, iconURL: string = "https://static.thenounproject.com/png/5018320-200.png"):Application => {
     const app:Application = {
-        id: Date.now().toString(),
+        id: crypto.randomUUID(),
         name: name,
         type: type,
         iconURL: iconURL,
--- src/utils/createIcon.ts
+++ src/utils/createIcon.ts
@@ -2,7 +2,7 @@ import { Icon } from "../types/Icon";
 
 const createIcon = (name: string, type: string, iconURL: string = "https://static.thenounproject.com/png/5018320-200.png"):Icon=> {
     const icon:Icon = {
-        id: Date.now().toString(),
+        id: crypto.randomUUID(),
         name: name,
         type: type,
         iconURL: iconURL,
