0a381b52d53eb6edcd3e1bb006912d856d6472f3
yurishe0 <yurishe00@gmail.com>
2025-02-19 23:37:35 +0100

    fix: hide submodal when it leaves hover area

--- src/components/ActionsModal/ActionsModal.tsx
+++ src/components/ActionsModal/ActionsModal.tsx
@@ -30,7 +30,7 @@ function ActionsModal({ actions, position, isSubModal = false }: { actions: Acti
 function ActionItem({ action }: { action: Action }) {
     const [isExpanded, setIsExpanded] = useState(false);
     return (
-        <div className={styles.action}>
+        <div className={styles.action} onMouseLeave={() => { setIsExpanded(false) }}>
             <div className={styles.iconContainer}></div>
             <button onClick={action.cb} onMouseOver={() => { setIsExpanded(true)}}> {action.name} </button>
             {isExpanded && action.options ? <ActionsModal actions={action.options} isSubModal={true} /> : null}
