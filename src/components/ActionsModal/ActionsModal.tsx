f2632bf749c4402c1626a98da29f38aba53c2002
yurishe0 <yurishe00@gmail.com>
2025-02-19 22:46:02 +0100

    style: adjust positioning for sub action modals

--- src/components/ActionsModal/ActionsModal.tsx
+++ src/components/ActionsModal/ActionsModal.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 type Action = {
     name: string;
     cb: () => void;
+    iconURL?: string;
     options?: Action[];
 }
 
@@ -13,23 +14,27 @@ type Position = {
 };
 
 
-function ActionsModal({ actions, position }: { actions: Action[], position: Position }) {
+function ActionsModal({ actions, position, isSubModal = false }: { actions: Action[], position?: Position, isSubModal: boolean }) {
     return (
-        <div className={styles.actionsModal} style={{ transform: `translate(${position.x}px, ${position.y}px)` }}>
-            { actions.map(action => (
+        <div
+            className={`${styles.actionsModal} ${isSubModal ? styles.subActionsModal : ''}`}
+            style={position ? { transform: `translate(${position.x}px, ${position.y}px)` } : undefined}
+        >
+            {actions.map(action => (
                 <ActionItem key={action.name} action={action} />
             ))}
         </div>
-    )
+    );
 }
 
-function ActionItem({ action } : { action: Action }) {
+function ActionItem({ action }: { action: Action }) {
     const [isExpanded, setIsExpanded] = useState(false);
     return (
-        <>
-            <button className={styles.action} onClick={action.cb} onMouseOver={() => { setIsExpanded(true)}}> {action.name} </button>
-            {isExpanded && action.options ? <ActionsModal actions={action.options} /> : null}
-        </>
+        <div className={styles.action}>
+            <div className={styles.iconContainer}></div>
+            <button onClick={action.cb} onMouseOver={() => { setIsExpanded(true)}}> {action.name} </button>
+            {isExpanded && action.options ? <ActionsModal actions={action.options} isSubModal={true} /> : null}
+        </div>
     )
 }
 
