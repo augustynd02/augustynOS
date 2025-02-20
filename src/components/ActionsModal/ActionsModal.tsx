84aa46d7254a542a1b5d3a35b13d6ec410aaeedc
yurishe0 <yurishe00@gmail.com>
2025-02-20 17:00:31 +0100

    feat: icon for expandable modal elements

--- src/components/ActionsModal/ActionsModal.tsx
+++ src/components/ActionsModal/ActionsModal.tsx
@@ -1,6 +1,8 @@
 import styles from './ActionsModal.module.scss';
 import { useState } from 'react';
 
+import { FaChevronRight } from "react-icons/fa6";
+
 type Action = {
     name: string;
     cb: () => void;
@@ -32,7 +34,7 @@ function ActionItem({ action }: { action: Action }) {
     return (
         <div className={styles.action} onMouseLeave={() => { setIsExpanded(false) }}>
             <div className={styles.iconContainer}></div>
-            <button onClick={action.cb} onMouseOver={() => { setIsExpanded(true)}}> {action.name} </button>
+            <button onClick={action.cb} onMouseOver={() => { setIsExpanded(true)}}> {action.name} { action.options && <FaChevronRight />}</button>
             {isExpanded && action.options ? <ActionsModal actions={action.options} isSubModal={true} /> : null}
         </div>
     )
