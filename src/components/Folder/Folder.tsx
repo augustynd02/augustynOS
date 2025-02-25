9dccbb133db9b66b5eaf1a1ba91dac568c3b09b6
yurishe0 <yurishe00@gmail.com>
2025-02-25 17:57:14 +0100

    feat: finish structure and styling of folder

--- src/components/Folder/Folder.tsx
+++ src/components/Folder/Folder.tsx
@@ -4,6 +4,7 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { FaArrowRight } from "react-icons/fa6";
 import { FaArrowUp } from "react-icons/fa6";
 import { BiSolidChevronDown } from "react-icons/bi";
+import { LiaSearchSolid } from "react-icons/lia";
 
 function Folder() {
     return (
@@ -13,17 +14,17 @@ function Folder() {
                 <button> <FaArrowRight /> </button>
                 <button> <BiSolidChevronDown /> </button>
                 <button> <FaArrowUp /> </button>
-                <div className="location">
-                    <img src="" alt="" />
+                <div className={styles.location}>
+                    <img src="https://winaero.com/blog/wp-content/uploads/2018/11/folder-icon-big-256.png" alt="" />
                     <input type="text" />
                 </div>
-                <div className="search">
-                    <input type="text" name="" id="" />
-                    <img src="" alt="" />
+                <div className={styles.search}>
+                    <input type="text" name="" id="" placeholder="Search..." />
+                    <LiaSearchSolid />
                 </div>
             </div>
             <div className={styles.content}>a</div>
-            <div className={styles.info}></div>
+            <div className={styles.info}>a</div>
         </div>
     )
 }
