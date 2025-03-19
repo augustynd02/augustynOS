1506625042b360f82b9959470b0d1f763811589b
yurishe0 <yurishe00@gmail.com>
2025-03-19 19:13:49 +0100

    feat: allow adjusting the file's type in the util function

--- src/utils/createFile.ts
+++ src/utils/createFile.ts
@@ -1,10 +1,10 @@
 import { File } from "../types/File";
 
-const createFile = (id: string, name: string, iconURL: string = "https://icon-library.com/images/windows-file-icon/windows-file-icon-16.jpg"): File => {
+const createFile = (id: string, name: string, type: string = 'file', iconURL: string = "https://icon-library.com/images/windows-file-icon/windows-file-icon-16.jpg"): File => {
     return {
         id: id,
         name: name,
-        type: "file",
+        type: type,
         iconURL: iconURL,
     }
 }
