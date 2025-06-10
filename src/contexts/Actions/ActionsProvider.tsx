8a0f1420b213230195f609697ea9a186f3fec7e2
yurishe0 <yurishe00@gmail.com>
2025-06-10 23:37:19 +0200

    refactor(ActionsProvider): wrap context methods in useCallback

--- src/contexts/Actions/ActionsProvider.tsx
+++ src/contexts/Actions/ActionsProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import ActionsContext from './ActionsContext';
 import { Action } from '../../types/Action';
 import { Position } from '../../types/Position';
@@ -8,18 +8,20 @@ export default function ActionsProvider({ children }: { children: React.ReactNod
     const [modalPosition, setModalPosition] = useState<Position>({ x: 0, y: 0 });
     const [actions, setActions] = useState<Action[]>([]);
 
-    const handleOpenModal = (e: React.MouseEvent, newActions: Action[]) => {
+    const handleOpenModal = useCallback((e: React.MouseEvent, newActions: Action[]) => {
         e.preventDefault();
         setIsModalShown(true);
         setModalPosition({ x: e.clientX, y: e.clientY });
         setActions(newActions);
-    };
+    }, []);
 
-    const handleCloseModal = () => setIsModalShown(false);
+    const handleCloseModal = useCallback(() => {
+        setIsModalShown(false);
+    }, []);
 
     return (
         <ActionsContext.Provider value={{ handleOpenModal, handleCloseModal, modalPosition, isModalShown, actions }}>
-            { children }
+            {children}
         </ActionsContext.Provider>
     )
 }
